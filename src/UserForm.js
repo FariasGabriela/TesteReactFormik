133b54233c818dba8e8a9398d2aa1fcd6e7cc0a9
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2018-09-09 22:02:42 -0300

    Acrescentado Yup

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('E-mail is not valid!')
+      .required('E-mail is required!')
+  })
 
 const UserForm = () => (
   <div>
     <h1>Sign Up</h1>
     <Formik
+      validationSchema={validationSchema} 
       initialValues={{
         firstName: '',
         lastName: '',
@@ -14,15 +22,16 @@ const UserForm = () => (
         console.log(values);
       }}
       render={() => (
+
         <Form>
-          <label htmlFor="firstName">Nome</label>
+          <label htmlFor="firstName" >Nome</label>
           <Field name="firstName" placeholder="Nome" />
 
           <label htmlFor="lastName">Sobrenome</label>
           <Field name="lastName" placeholder="Sobrenome" />
 
-          <label htmlFor="email">Email</label>
-          <Field name="email" placeholder="email@local.com" type="email" />
+          <label htmlFor="email" >Email</label>
+          <Field name="email" placeholder="email@local.com" />
 
           <button type="submit">Submit</button>
         </Form>
